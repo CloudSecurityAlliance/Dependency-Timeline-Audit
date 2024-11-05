a72229ed8f6fbbb2cb13a8fb022be5d38f8061fc
Josh Buker <crypto@joshbuker.com>
2024-11-05 12:23:17 -0800

    Fix TextFormatter not using class methods

--- ruby/lib/dependency-timeline-audit/text_format.rb
+++ ruby/lib/dependency-timeline-audit/text_format.rb
@@ -1,49 +1,51 @@
 module DependencyTimelineAudit
   class TextFormat
-    def color=(color)
-      case color
-      when :red
-        print red
-      when :green
-        print green
-      when :yellow
-        print yellow
-      else
-        raise ArgumentError, "Unknown color: #{color}"
+    class << self
+      def color=(color)
+        case color
+        when :red
+          print red
+        when :green
+          print green
+        when :yellow
+          print yellow
+        else
+          raise ArgumentError, "Unknown color: #{color}"
+        end
       end
-    end
 
-    def style=(style)
-      case style
-      when :bold
-        print bold
-      else
-        raise ArgumentError, "Unknown style: #{style}"
+      def style=(style)
+        case style
+        when :bold
+          print bold
+        else
+          raise ArgumentError, "Unknown style: #{style}"
+        end
       end
-    end
 
-    def reset!
-      print reset
-    end
+      def reset!
+        print reset
+      end
 
-    def reset
-      "\e[0m"
-    end
+      def reset
+        "\e[0m"
+      end
 
-    def bold
-      "\e[1m"
-    end
+      def bold
+        "\e[1m"
+      end
 
-    def red
-      "\e[31m"
-    end
+      def red
+        "\e[31m"
+      end
 
-    def green
-      "\e[32m"
-    end
+      def green
+        "\e[32m"
+      end
 
-    def yellow
-      "\e[33m"
+      def yellow
+        "\e[33m"
+      end
     end
   end
 end
